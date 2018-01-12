54714d1add496e4cecbd7e99cbacb95fbcc4a69b
tingweiwu <tiffanywutingwei@gmail.com>
2018-01-12 11:39:52 -0500

    final-upload15

--- js/script.js
+++ js/script.js
@@ -242,20 +242,4 @@ function hidePreloader() {
 })
 
 
-/*-------------------------------------------
-Google Analytics
----------------------------------------------*/
-
-if window.history?.pushState and window.history.replaceState
-  document.addEventListener 'page:change', (event) =>
-
-    # Google Analytics
-    if window.ga != undefined
-      ga('set', 'location', location.href.split('#')[0])
-      ga('send', 'pageview', {"title": document.title})
-    else if window._gaq != undefined
-      _gaq.push(['_trackPageview'])
-    else if window.pageTracker != undefined
-      pageTracker._trackPageview();
-
 //End
