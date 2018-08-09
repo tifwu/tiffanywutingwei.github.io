76ca37e7c38eaca14919876be6e3b085d1ae2346
tingweiwu <tiffanywutingwei@gmail.com>
2018-08-08 22:27:23 -0700

    new-portfolio5

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@
             // Make sure this.hash has a value before overriding default behavior
             if (this.hash !== "") {
                 // Prevent default anchor click behavior
-                event.preventDefault();
+                //event.preventDefault();
 
                 // Store hash
                 var hash = this.hash;
@@ -169,6 +169,8 @@
             } // End if
         });
     });
+
+
 
 
 }());
