e0035a56f6e4907631fdbb6b8864edb96c09153f
tingweiwu <tiffanywutingwei@gmail.com>
2018-08-08 22:43:46 -0700

    new-portfolio7

--- js/main.js
+++ js/main.js
@@ -163,17 +163,20 @@
                 // Using jQuery's animate() method to add smooth page scroll
                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 
-                if (nav.length) {
+
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+
+                /*if (nav.length) {
                     var contentNav = nav.offset().top;
 
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                    }, 800, function(){
 
-                        // Add hash (#) to URL when done scrolling (default click behavior)
-                        window.location.hash = hash;
-                    });
-                }
+                }*/
 
             } // End if
         });
