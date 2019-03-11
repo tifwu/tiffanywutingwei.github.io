9cbf6b8eb2b80fc780a05141e73b5a52126e39ae
tiffanywutingwei <tiffanywutingwei@gmail.com>
2019-03-10 21:25:58 -0400

    20190309-2
    header

--- js/main.js
+++ js/main.js
@@ -146,50 +146,98 @@
 //    var nav = $('.content-nav');
     // Hide Header on on scroll down
 
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    const nav = document.querySelector('#fh5co-header-section');
-    var navbarHeight = $('header').outerHeight();
+//    var didScroll;
+//    var lastScrollTop = 0;
+//    var delta = 5;
+//    const nav = document.querySelector('#fh5co-header-section');
+//    var navbarHeight = $('header').outerHeight();
+//    
+//
+//    $(window).scroll(function(event){
+//        didScroll = true;
+//    });
+//
+//    setInterval(function() {
+//        if (didScroll) {
+//            hasScrolled();
+//            didScroll = false;
+//        }
+//    }, 250);
+//
+//    function hasScrolled() {
+//        var st = document.documentElement.scrollTop;
+//        //console.log(st, lastScrollTop, navbarHeight);
+//        // Make sure they scroll more than delta
+//        if(Math.abs(lastScrollTop - st) <= delta)
+//            return;
+//        
+//
+//        // If they scrolled down and are past the navbar, add class .nav-up.
+//        // This is necessary so you never see what is "behind" the navbar.
+////        console.log('has scroll');
+//        if (st > lastScrollTop && st > navbarHeight){
+//            // Scroll Down
+////            $('header').addClass('nav-up');
+//            nav.className = 'nav-up';
+//        } else {
+//            // Scroll Up
+//            if(st + $(window).height() < $(document).height()) {
+//                nav.className = '';
+//                nav.style.background = 'white';
+//                //$('header').removeClass('nav-up');
+//            }   
+//        }
+//
+//        lastScrollTop = st;
+//    }
     
 
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
 
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
-    function hasScrolled() {
-        var st = document.documentElement.scrollTop;
-        //console.log(st, lastScrollTop, navbarHeight);
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-        
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        console.log('has scroll');
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down
-//            $('header').addClass('nav-up');
-            nav.className = 'nav-up';
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                nav.className = '';
-                nav.style.background = 'white';
-                //$('header').removeClass('nav-up');
-            }   
-        }
-
-        lastScrollTop = st;
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
     }
+}, 250);
+
+function hasScrolled() {
+    var st = $(window).scrollTop();
+    
+    console.log(st, lastScrollTop, navbarHeight);
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+         console.log("add class");
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+         console.log("remove class");
+        $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta) {
+        console.log("return");
+        return;
+    }
+    
+    lastScrollTop = st;   
+    
+}
 
     //smooth page scroll.
     $(document).ready(function(){
